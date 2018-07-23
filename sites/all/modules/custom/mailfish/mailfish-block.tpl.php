0eb1578aa3a3d63e0d91ef99a942d1c4c630b2d6
shathiso <shathisontibi@yahoo.com>
2018-07-23 21:01:01 +0200

    node subscription module (mailfish)

---
+++ sites/all/modules/custom/mailfish/mailfish-block.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+*
+*/
+?>
+
+<div id = 'mailfish-rocks'>
+   <?php print t('Mailfish Rocks'); ?>
+   <?php print $form; ?>
+</div>
---
+++ sites/all/modules/custom/practice_block/practice_block_template.tpl.php
@@ -0,0 +1,2 @@
+<div class="text_one"> <?php print $text_one ?> </div>
+<div class="text_two"> <?php print $text_two ?> </div>
